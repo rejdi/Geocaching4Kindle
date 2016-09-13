726151beda5966a23a5e69f1037df5d507e1d999
Martin Rejda <rejdi@ksp.sk>
2016-09-13 07:06:33 +0200

    Fixed login.

--- lib/login.php
+++ lib/login.php
@@ -4,19 +4,38 @@ function login($username, $password, $session_id) {
 	//TODO: skontrolovat username a password na ine znaky ako a-Z0-1
 	$cookiefile = 'result/' . $session_id . '/cookie.txt';
 	$postfile = 'result/' . $session_id . '/post.txt';
+	$tmp = 'result/' . $session_id . '/result.html';
+	
+	//token
+	$command = "wget --keep-session-cookies -e robots=off --save-cookies ".$cookiefile." --referer='https://www.geocaching.com/account/login?returnUrl=%2fplay' 'https://www.geocaching.com/account/login?returnUrl=%2fplay' -O " . $tmp . " -o /dev/null";
+	exec(escapeshellcmd($command));
+	
+	$data = file_get_contents($tmp);
+	$needle = '__RequestVerificationToken" type="hidden" value="';
+	$token_start=stripos($data, $needle);
+	$token = substr($data, $token_start+strlen($needle), 92);
+	
+	$data = http_build_query(
+		array(
+			'__RequestVerificationToken' => $token,
+			'Username' => $username,
+			'Password' => $password
+			)
+		);
 	
 	/*html*/
-	$res = file_put_contents($postfile, "__EVENTTARGET=&ctl00%24ContentBody%24btnSignIn=Sign%20In&ctl00%24ContentBody%24cbRememberMe=on&ctl00%24ContentBody%24tbPassword=".$password."&ctl00%24ContentBody%24tbUsername=".$username);
+	$res = file_put_contents($postfile, $data);
 	if (!$res) {
 		return false;
 	}
 	
-	$command = "wget --post-file='".$postfile."' --keep-session-cookies -e robots=off --save-cookies ".$cookiefile." --referer='https://www.geocaching.com/login/default.aspx' 'https://www.geocaching.com/login/default.aspx' -O /dev/null -o /dev/null";
+	$command = "wget --post-file='".$postfile."' --keep-session-cookies -e robots=off --load-cookies ".$cookiefile." --save-cookies ".$cookiefile." --referer='https://www.geocaching.com/account/login?returnUrl=%2fplay' 'https://www.geocaching.com/account/login?returnUrl=%2fplay' -O " . $tmp . " -o /dev/null";
 	exec(escapeshellcmd($command));
 	
 	$cookies = file_get_contents($cookiefile);
-	$userid = stristr($cookies, 'userid');
+	$userid = stristr($cookies, 'gspkauth');
 	unlink($postfile);
+	unlink($tmp);
 	if (empty($userid)) {
 		unlink($cookiefile);
 		return false;
